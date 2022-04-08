337b714e38f67a4912ac1daf93d76829ce186cb2
Ken <kenblackuoo@gmail.com>
2022-04-08 17:29:28 -0400

    Brief syntax update

--- frontend/src/components/GoogleMaps/LionGateMap.js
+++ frontend/src/components/GoogleMaps/LionGateMap.js
@@ -22,7 +22,7 @@ class LionGateMap extends React.Component {
           
         return(
         <MapWithAMarker
-        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAdK3c1JlvzeLj5rDD5NhOgD_Gr11VcDE4&v=3.exp&libraries=geometry,drawing,places"
+        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places"
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `350px` }} />}
         mapElement={<div style={{ height: `100%` }} />}
