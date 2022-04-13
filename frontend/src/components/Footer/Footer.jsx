4859b1766629e95838b8d00eb968241fcbd2034f
Ken <kenblackuoo@gmail.com>
2022-04-13 13:19:04 -0400

    Updated styling to comment and reply forms + Added another icon to the footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Footer.css";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -8,7 +9,8 @@ const Footer = () => {
             <medium className='copy-color'> Ken Blackwell's </medium><medium className="footer-color"> HoCo Pickup &copy;{new Date().getFullYear()} | No Rights Reserved | Open-Source</medium>
       </div>
       <nav>
-        <li className = "footer-list"><a href="https://ballislife.com/" target="_blank"><img src ="/images/ballislife.jpg" className = "footer-image" alt = "ball is life"/></a></li>
+        <li className = "footer-list foot-lo"><Link to = '/'><img src ="/images/basketball-logo-wh.jpg" className = "footer-image" alt = "logo-basketball"/></Link></li>
+        <li className = "footer-list foot-bl"><a href="https://ballislife.com/" target="_blank"><img src ="/images/ballislife.jpg" className = "footer-image" alt = "ball is life"/></a></li>
         <li className = "footer-list"><a href="https://www.slamonline.com/" target="_blank"><img src ="/images/slam.jpg" className = "footer-image" alt = "slam"/></a></li>
       </nav>
     </footer>
