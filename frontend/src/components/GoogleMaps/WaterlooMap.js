56d06c4895ee81b7578720c7834d3e7dea6894ad
Ken <kenblackuoo@gmail.com>
2022-04-15 13:11:17 -0400

    Minor styling updates to Courts pages

--- frontend/src/components/GoogleMaps/WaterlooMap.js
+++ frontend/src/components/GoogleMaps/WaterlooMap.js
@@ -25,7 +25,7 @@ class WaterlooMap extends React.Component {
         googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places"
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `300px` }} />}
-        mapElement={<div style={{ height: `100%`, borderRadius: `20px`, boxShadow:  '4px 2px 5px black' }} />}
+        mapElement={<div style={{ height: `100%`, borderRadius: `20px`, boxShadow:  '4px 2px 5px black', width: `500px` }} />}
 />
         )
     }
